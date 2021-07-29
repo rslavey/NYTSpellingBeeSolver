bc83560369d95dcab6946fd2b5a833b3e4e6a69e
Randy Slavey <randy.slavey@dgslaw.com>
2021-07-28 18:31:42 -0600

    refactor

--- SpellingBeeSolver/Program.cs
+++ SpellingBeeSolver/Program.cs
@@ -18,44 +18,11 @@ namespace SpellingBeeSolver
             Console.Write($"Minimum Word Length: ");
             var wl = int.Parse(Console.ReadLine());
             Console.WriteLine();
-            var wordList = new List<DictEntry>();
-            Dictionary<string, int> wordKVP = new();
+            
             foreach (var f in Directory.GetFiles(ConfigurationManager.AppSettings.Get("dictionaryPath")))
             {
                 Console.WriteLine(string.Join(", ", File.ReadAllLines(f).Select(w => w.ToLower().Trim()).Where(w => w.All(x => al.Contains(x)) && w.Any(x => x == rl) && w.Length >= wl).Select(w => $"{w}")));
-                
-                //foreach (var w in File.ReadAllLines(f).Select(w => w.ToLower().Trim()))
-                //{
-                //    if (w.All(x => al.Contains(x)) &&
-                //        w.Any(x => x == rl) &&
-                //        w.Length >= wl)
-                //    {
-                //        if (wordKVP.ContainsKey(w))
-                //        {
-                //            wordKVP[w]++;
-                //        }
-                //        else
-                //        {
-                //            wordKVP.Add(w, 1);
-                //        }
-                //    }
-                //}
-
             }
-
-            //Console.WriteLine(
-            //    string.Join(", ", wordKVP
-            //        .OrderByDescending(x => x.Value).ThenBy(x => x.Key).Select(x => $"{x.Key} ({x.Value})")
-            //        )
-            //    );
-        }
-
-        public class DictEntry
-        {
-            public string Word { get; set; }
-            public int NumLetters { get; set; }
-            public string UniqueLettersOrdered { get; set; }
-            public int Instances { get; set; }
         }
     }
 }
